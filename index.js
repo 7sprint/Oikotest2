4b116d33f922355c726740e25300b96c8f6a86c2
7sprint <79527743+7sprint@users.noreply.github.com>
2021-02-23 18:03:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! This is the Oikonomika HR application IN THE CLOUD");
+    response.writeHead(400, {"Content-Type": "text/plain"});
+    response.end("Hello World! qsdfqsdfqsdfqsdf qsdfThis is the Oikonomika HR application IN THE CLOUD");
 });
 
 const port = process.env.PORT || 1337;
